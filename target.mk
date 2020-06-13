89af7b43f9a2594c2db7530c4c46e10112de21fc
u0225 <qq3842126@gmail.com>
2020-06-13 19:51:08 +0800

    update

--- target.mk
+++ target.mk
@@ -14,16 +14,18 @@ DEVICE_TYPE?=router
 
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd luci luci-compat wget \
-block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw libustream-openssl ca-certificates \
 default-settings luci-app-ddns luci-app-wol luci-app-vlmcsd luci-app-ramfree luci-app-flowoffload \
 luci-app-passwall luci-app-timecontrol luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl luci-app-fileassistant \
 luci-app-pptp-vpnserver-manyusers luci-app-pppoe-server luci-app-pppoe-relay luci-theme-bootstrap-mod luci-theme-darkmatter \
-luci-app-sfe luci-app-nlbwmon luci-app-accesscontrol luci-app-cpufreq \
+block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw libustream-openssl ca-certificates \
+luci-app-upnp luci-app-adbyby-plus luci-app-autoreboot \
+luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic \
+luci-app-arpbind luci-app-sfe luci-app-nlbwmon luci-app-accesscontrol luci-app-cpufreq \
 ddns-scripts_aliyun ddns-scripts_dnspod
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
-DEFAULT_PACKAGES.router:=dnsmasq-full iptables ip6tables ppp ppp-mod-pppoe firewall odhcpd-ipv6only odhcp6c kmod-ipt-offload
+DEFAULT_PACKAGES.router:=dnsmasq-full iptables ip6tables ppp ppp-mod-pppoe firewall odhcpd-ipv6only odhcp6c kmod-ipt-offload kmod-tcp-bbr
 DEFAULT_PACKAGES.bootloader:=
 
 ifneq ($(DUMP),)
